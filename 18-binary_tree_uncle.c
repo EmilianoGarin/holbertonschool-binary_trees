6f5ebb2f08935fb41c7fbfb32349cec814650ee8
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-05-15 22:17:31 -0700

    fix 17 and 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,9 +8,12 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || node->parent || node->parent->parent)
-		return (NULL);
-	if (node->parent == node->parent->parent->left)
-		return (node->parent->parent->right);
-	return (node->parent->parent->left);
+	if (node && node->parent && node->parent->parent)
+	{
+		if (node->parent == node->parent->parent->left)
+			return (node->parent->parent->right);
+		else
+			return (node->parent->parent->left);
+	}
+	return (NULL);
 }
